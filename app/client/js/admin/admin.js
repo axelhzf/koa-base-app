11aa92ef1dd7303659bcb212357c7b7d2bafcd17
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-07-31 16:58:56 +0100

    admin-panel

--- app/client/js/admin/admin.js
+++ app/client/js/admin/admin.js
@@ -5,16 +5,24 @@ require("angular-route");
 var app = angular.module("admin", ["ngRoute"]);
 
 var UsersController = require("./controllers/UsersController");
+var DashboardController = require("./controllers/DashboardController");
 
 app.controller("UsersController", UsersController);
+app.controller("DashboardController", DashboardController);
 
 app.config(function ($routeProvider) {
-  $routeProvider.when('/', {
-    controller: 'UsersController',
-    template: require("./templates/users.jade")
-  }).otherwise({
-    redirectTo: '/'
-  });
+  $routeProvider
+    .when('/', {
+      controller: 'DashboardController',
+      template: require("./templates/dashboard.jade")
+    })
+    .when('/users', {
+      controller: 'UsersController',
+      template: require("./templates/users.jade")
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 });
 
 
